c854270bd0683fc7d766d4a047acdbe62d9d60da
Lucius <jilavuluc@gmail.com>
2020-08-10 20:09:10 +0200

    Added bzip2 + fixed the mistakes

--- packages/bzip2/pre.install.sh
+++ packages/bzip2/pre.install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget http://www.linuxfromscratch.org/patches/lfs/9.1/bzip2-1.0.8-install_docs-1.patch
-patch -Np1 -i ../bzip2-1.0.8-install_docs-1.patch
+patch -Np1 -i ./bzip2-1.0.8-install_docs-1.patch
 sed -i 's@\(ln -s -f \)$(PREFIX)/bin/@\1@' Makefile
 sed -i "s@(PREFIX)/man@(PREFIX)/share/man@g" Makefile
 make -f Makefile-libbz2_so

098cb8070cf17fbc57f98175d48167b68497259f
Lucius <jilavuluc@gmail.com>
2020-08-12 12:59:19 +0200

    Added Zstd-1.4.4 (￣￢￣*)
    Co-Authored-By: Lucius <62286562+lucius450@users.noreply.github.com>

---
+++ packages/zstd/do.install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+make prefix=/usr install
---
+++ packages/zstd/post.update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -v /usr/lib/libzstd.a
+mv -v /usr/lib/libzstd.so.* /lib
+ln -sfv ../../lib/$(readlink /usr/lib/libzstd.so) /usr/lib/libzstd.so

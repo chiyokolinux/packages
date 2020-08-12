3f7c84707ce24406b6f7ecc30dceeed3663d4e2b
Lucius <jilavuluc@gmail.com>
2020-08-12 13:40:45 +0200

    Added Kbd-2.2.0 (◕‿◕✿)
    Co-Authored-By: Lucius <62286562+lucius450@users.noreply.github.com>

---
+++ packages/kbd/pre.install.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash
+wget http://www.linuxfromscratch.org/patches/lfs/9.1/kbd-2.2.0-backspace-1.patch
+patch -Np1 -i ../kbd-2.2.0-backspace-1.patch
+sed -i 's/\(RESIZECONS_PROGS=\)yes/\1no/g' configure
+sed -i 's/resizecons.8 //' docs/man/man8/Makefile.in

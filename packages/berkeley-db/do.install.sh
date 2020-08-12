e6a947c7999575198011df7e20aca7618d1eca45
Lucius <jilavuluc@gmail.com>
2020-08-12 18:58:25 +0200

    Added Berkeley DB-5.3.28

---
+++ packages/berkeley-db/do.install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+make docdir=/usr/share/doc/db-5.3.28 install &&
+
+chown -v -R root:root                        \
+      /usr/bin/db_*                          \
+      /usr/include/db{,_185,_cxx}.h          \
+      /usr/lib/libdb*.{so,la}                \
+      /usr/share/doc/db-5.3.28

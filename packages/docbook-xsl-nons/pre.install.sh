68f90d47a13b9887463c2e4642baaef47cbb5403
Lucius <jilavuluc@gmail.com>
2020-08-21 18:22:49 +0200

    Added some packages

---
+++ packages/docbook-xsl-nons/pre.install.sh
@@ -0,0 +1,3 @@
+ #!/bin/bash
+wget http://www.linuxfromscratch.org/patches/blfs/9.1/docbook-xsl-nons-1.79.2-stack_fix-1.patch
+patch -Np1 -i ../docbook-xsl-nons-1.79.2-stack_fix-1.patch
---
+++ packages/unzip/do.install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+make prefix=/usr MANDIR=/usr/share/man/man1 \
+ -f unix/Makefile install

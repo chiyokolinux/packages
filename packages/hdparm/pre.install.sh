5ca811efc138e96a1cedd5b4df61b805cf306c4a
Jonas Jaguar <36363495+jonasjaguar@users.noreply.github.com>
2020-09-20 18:44:49 +0200

    Added hdparm-9.58

---
+++ packages/hdparm/pre.install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+RED='\033[0;31m'
+BOLDRED='\033[1;31m'
+NC='\033[0m'
+printf "${BOLDRED}WARNING!\n${RED}As well as being useful, incorrect usage of Hdparm can destroy your information and in rare cases, drives. Use with caution and make sure you know what you are doing. If in doubt, it is recommended that you leave the default kernel parameters alone.${NC}\n"

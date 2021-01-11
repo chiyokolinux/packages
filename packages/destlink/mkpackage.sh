eba033a93d8d777cea64dff220a9825417580fa4
Emily <36363495+jonasjaguar@users.noreply.github.com>
2021-01-11 12:29:13 +0100

    Added destlink-0.1

---
+++ packages/destlink/mkpackage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "[    destlink    ] updating destlink..."
+
+# fetch latest github release version
+PKG_VERSION=$(curl -s "https://api.github.com/repos/chiyokolinux/destlink/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+
+echo "[    destlink    ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/destlink/package.conf
+
+echo "[    destlink    ] updated package destlink to version ${PKG_VERSION}"

5d59dd28e352315b85a1869a0648bfd2e7def42b
Emily <36363495+jonasjaguar@users.noreply.github.com>
2021-01-11 12:22:58 +0100

    Added cbsh-0.1

---
+++ packages/cbsh/mkpackage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "[      cbsh      ] updating cbsh..."
+
+# fetch latest github release version
+PKG_VERSION=$(curl -s "https://api.github.com/repos/chiyokolinux/cbsh/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+
+echo "[      cbsh      ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/cbsh/package.conf
+
+echo "[      cbsh      ] updated package cbsh to version ${PKG_VERSION}"

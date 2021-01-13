327b1ef9a48e61ab9608c6b419ae10b300680b49
Emily <36363495+jonasjaguar@users.noreply.github.com>
2021-01-13 19:22:40 +0100

    add ichirou scripts

--- packages/ichirou/mkpackage.sh
+++ packages/ichirou/mkpackage.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "nothing to be done for ichirou"
+echo "[    ichirou     ] updating ichirou..."
+
+# fetch latest github release version
+PKG_VERSION=$(curl -s "https://api.github.com/repos/chiyokolinux/ichirou/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+
+echo "[    ichirou     ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/ichirou/package.conf
+
+echo "[    ichirou     ] updated package ichirou to version ${PKG_VERSION}"

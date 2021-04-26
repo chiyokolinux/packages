e532bc23ee2bc70b43fe4d131d6853eb85b54a19
Emily <36363495+em1lyy@users.noreply.github.com>
2021-04-26 18:02:00 +0200

    auth to github api
    was afraid that my build server would run into rate limits
    if you do not want to authenticate, just don't set the
    environment variables

--- packages/destlink/mkpackage.sh
+++ packages/destlink/mkpackage.sh
@@ -2,7 +2,7 @@
 echo "[    destlink    ] updating destlink..."
 
 # fetch latest github release version
-PKG_VERSION=$(curl -s "https://api.github.com/repos/chiyokolinux/destlink/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/chiyokolinux/destlink/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
 
 echo "[    destlink    ] found version ${PKG_VERSION}, updating package.conf..."
 
--- packages/nezumi/mkpackage.sh
+++ packages/nezumi/mkpackage.sh
@@ -2,7 +2,7 @@
 echo "[     nezumi     ] updating nezumi..."
 
 # fetch latest github release version
-PKG_VERSION=$(curl -s "https://api.github.com/repos/chiyokolinux/nezumi/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/chiyokolinux/nezumi/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
 
 echo "[     nezumi     ] found version ${PKG_VERSION}, updating package.conf..."
 

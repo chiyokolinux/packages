f96f54a140d8b16865e2c40c39c3226fde27ff3e
Emily <36363495+em1lyy@users.noreply.github.com>
2021-07-25 11:57:50 +0200

    write mkpackage.sh for nano

--- packages/nano/mkpackage.sh
+++ packages/nano/mkpackage.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "nothing to be done for nano"
+echo "[      nano      ] updating nano..."
+
+# fetch latest nano-editor.org (v5) release version
+PKG_VERSION=$(curl -s "https://www.nano-editor.org/dist/v5/" | grep -o "nano-[0-9].[0-9]*.tar.xz" | uniq | sort | tail -n1 | grep -o "[0-9]\.[0-9]*")
+
+echo "[      nano      ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/nano/package.conf
+
+echo "[      nano      ] updated package nano to version ${PKG_VERSION}"

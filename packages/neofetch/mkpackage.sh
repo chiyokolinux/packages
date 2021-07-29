239958c2c3f9e7972baaaf0c1ffd7eb6d73dfa5f
Emily <36363495+em1lyy@users.noreply.github.com>
2021-07-29 23:43:19 +0200

    neofetch mkpackage.sh (github releases)

--- packages/neofetch/mkpackage.sh
+++ packages/neofetch/mkpackage.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "nothing to be done for neofetch"
+echo "[    neofetch    ] updating neofetch..."
+
+# fetch latest github release version
+PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/dylanaraps/neofetch/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+
+echo "[    neofetch    ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/neofetch/package.conf
+
+echo "[    neofetch    ] updated package neofetch to version ${PKG_VERSION}"

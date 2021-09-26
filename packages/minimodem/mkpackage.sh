3cde8123d50e5ef147d17feba38e1c5292d2d0ab
Emily <36363495+em1lyy@users.noreply.github.com>
2021-09-26 16:38:01 +0200

    minimodem mkpackage.sh

--- packages/minimodem/mkpackage.sh
+++ packages/minimodem/mkpackage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "[    ichirou     ] updating ichirou..."
+echo "[   minimodem    ] updating minimodem"
 
 # fetch latest github release version
-PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/chiyokolinux/ichirou/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/kamalmostafa/minimodem/tags" | grep " *\"name\":" | head -n1 | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
 
-echo "[    ichirou     ] found version ${PKG_VERSION}, updating package.conf..."
+echo "[   minimodem    ] found version ${PKG_VERSION}, updating package.conf..."
 
-sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/ichirou/package.conf
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/minimodem/package.conf
 
-echo "[    ichirou     ] updated package ichirou to version ${PKG_VERSION}"
+echo "[   minimodem    ] updated package minimodem to version ${PKG_VERSION}"

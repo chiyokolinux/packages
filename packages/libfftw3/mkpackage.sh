35861d2add1d8919b55bfe22c4aa40442cd51cb7
Emily <36363495+em1lyy@users.noreply.github.com>
2021-09-26 17:10:27 +0200

    libfftw3 mkpackage.sh

--- packages/libfftw3/mkpackage.sh
+++ packages/libfftw3/mkpackage.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "nothing to be done for libfftw3"
+echo "[    libfftw3    ] updating libfftw3"
+
+# fetch latest fftw.org (3.x.x) release version
+PKG_VERSION=$(curl -s "https://www.fftw.org/download.html" | grep -o "fftw-3.[0-9]+.[0-9]+.tar.gz" | uniq | sort | tail -n1 | grep -o "3.[0-9]+.[0-9]+")
+
+echo "[    libfftw3    ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/libfftw3/package.conf
+
+echo "[    libfftw3    ] updated package libfftw3 to version ${PKG_VERSION}"

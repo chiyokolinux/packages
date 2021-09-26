1cee8ccb323d5d939fdfdef35bf97822cd65d292
Emily <36363495+em1lyy@users.noreply.github.com>
2021-09-26 17:14:02 +0200

    libfftw3l mkpackage.sh

--- packages/libfftw3l/mkpackage.sh
+++ packages/libfftw3l/mkpackage.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "nothing to be done for libfftw3l"
+echo "[    libfftw3l   ] updating libfftw3l"
+
+# just follow main libfftw3 package version
+PKG_VERSION=$(grep -o "^VERSION=[0-9\.\-]*$" ../libfftw3/package.conf | sed "s/VERSION=//")
+
+echo "[    libfftw3l   ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/libfftw3l/package.conf
+
+echo "[    libfftw3l   ] updated package libfftw3l to version ${PKG_VERSION}"

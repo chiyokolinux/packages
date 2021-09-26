4662f26f91e2694d280af37c4473029fff465192
Emily <36363495+em1lyy@users.noreply.github.com>
2021-09-26 16:35:29 +0200

    add minimodem

---
+++ packages/minimodem/mkpackage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "[    ichirou     ] updating ichirou..."
+
+# fetch latest github release version
+PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/chiyokolinux/ichirou/releases/latest" | grep tag_name | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+
+echo "[    ichirou     ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/ichirou/package.conf
+
+echo "[    ichirou     ] updated package ichirou to version ${PKG_VERSION}"

7b1f6a2341b8a93a06d855e63170ee4c24f7469b
Emily <36363495+em1lyy@users.noreply.github.com>
2021-11-08 18:45:10 +0100

    ne mkpackage.sh

--- packages/ne/mkpackage.sh
+++ packages/ne/mkpackage.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "nothing to be done for ne"
+echo "[       ne       ] updating ne..."
+
+# fetch latest github tag version
+PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/vigna/ne/tags" | grep " *\"name\":" | head -n1 | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+
+echo "[       ne       ] found version ${PKG_VERSION}, updating package.conf..."
+
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/ne/package.conf
+
+echo "[       ne       ] updated package ne to version ${PKG_VERSION}"

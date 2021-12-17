35a31bff907451b4449dc196b368eb11bf383679
Emily <36363495+em1lyy@users.noreply.github.com>
2021-12-17 23:49:29 +0100

    berkeley-db install prefix

--- packages/berkeley-db/do.install.sh
+++ packages/berkeley-db/do.install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 make docdir=/usr/share/doc/db-5.3.28 install 
 
-chown -v -R root:root                        \
-      /usr/bin/db_*                          \
-      /usr/include/db{,_185,_cxx}.h          \
-      /usr/lib/libdb*.{so,la}                \
-      /usr/share/doc/db-5.3.28
+chown -v -R root:root                         \
+      ${DESTDIR}/usr/bin/db_*                 \
+      ${DESTDIR}/usr/include/db{,_185,_cxx}.h \
+      ${DESTDIR}/usr/lib/libdb*.{so,la}       \
+      ${DESTDIR}/usr/share/doc/db-5.3.28

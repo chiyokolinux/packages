cece0544855dcf34f60337c7777290da2e36371b
Emily <36363495+em1lyy@users.noreply.github.com>
2021-12-21 19:18:00 +0200

    attr correct destdir

---
+++ packages/attr/do.install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+make install
+mv -v ${DESTDIR}/usr/lib/libattr.so.* ${DESTDIR}/lib
+ln -sfv ../../lib/$(readlink ${DESTDIR}/usr/lib/libattr.so) ${DESTDIR}/usr/lib/libattr.so
--- packages/attr/post.install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mv -v /usr/lib/libattr.so.* /lib
-ln -sfv ../../lib/$(readlink /usr/lib/libattr.so) /usr/lib/libattr.so

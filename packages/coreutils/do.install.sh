e8595154a41d6a8253a50fbe27b13704f33987ff
Emily <36363495+em1lyy@users.noreply.github.com>
2021-12-25 23:16:06 +0100

    fix up coreutils

---
+++ packages/coreutils/do.install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+make install
+
+mv -v ${DESTDIR}/usr/bin/{cat,chgrp,chmod,chown,cp,date,dd,df,echo} ${DESTDIR}/bin
+mv -v ${DESTDIR}/usr/bin/{false,ln,ls,mkdir,mknod,mv,pwd,rm} ${DESTDIR}/bin
+mv -v ${DESTDIR}/usr/bin/{rmdir,stty,sync,true,uname} ${DESTDIR}/bin
+mv -v ${DESTDIR}/usr/bin/chroot ${DESTDIR}/usr/sbin
+mv -v ${DESTDIR}/usr/share/man/man1/chroot.1 ${DESTDIR}/usr/share/man/man8/chroot.8
+sed -i s/\"1\"/\"8\"/1 ${DESTDIR}/usr/share/man/man8/chroot.8
+mv -v ${DESTDIR}/usr/bin/{head,nice,sleep,touch} ${DESTDIR}/bin

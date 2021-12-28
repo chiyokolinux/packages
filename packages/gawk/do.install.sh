29538c759e065ae0df7880111bf8a7df73f45d0b
Emily <36363495+em1lyy@users.noreply.github.com>
2021-12-28 23:22:15 +0100

    fix gawk destdir

--- packages/gawk/do.install.sh
+++ packages/gawk/do.install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make install
-mkdir -v /usr/share/doc/gawk-5.0.1
-cp    -v doc/{awkforai.txt,*.{eps,pdf,jpg}} /usr/share/doc/gawk-5.0.1
+mkdir -v ${DESTDIR}/usr/share/doc/gawk-5.0.1
+cp -v doc/{awkforai.txt,*.{eps,pdf,jpg}} ${DESTDIR}/usr/share/doc/gawk-5.0.1

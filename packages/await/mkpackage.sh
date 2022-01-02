a0a279253d236d1806731b65a8e946bc4ab47453
Emily <36363495+em1lyy@users.noreply.github.com>
2022-01-02 16:37:00 +0200

    actual await

--- packages/await/mkpackage.sh
+++ packages/await/mkpackage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "[      ssu       ] updating ssu..."
+echo "[     await      ] updating await..."
 
 # fetch latest github release version
-PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/illiliti/ssu/tags" | grep " *\"name\":" | head -n1 | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
+PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/slavaGanzin/await/tags" | grep " *\"name\":" | head -n1 | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
 
-echo "[      ssu       ] found version ${PKG_VERSION}, updating package.conf..."
+echo "[     await    ] found version ${PKG_VERSION}, updating package.conf..."
 
-sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/ssu/package.conf
+sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/await/package.conf
 
-echo "[      ssu       ] updated package ssu to version ${PKG_VERSION}"
+echo "[     await      ] updated package await to version ${PKG_VERSION}"

4cd4b296f504e7b266e8bc352c2ec80e9a511589
Emily <36363495+em1lyy@users.noreply.github.com>
2022-01-23 00:53:36 +0100

    fix await alignment

--- packages/await/mkpackage.sh
+++ packages/await/mkpackage.sh
@@ -4,7 +4,7 @@ echo "[     await      ] updating await..."
 # fetch latest github release version
 PKG_VERSION=$(curl -u "${GITHUB_USER_NAME}:${GITHUB_API_TOKEN}" -s "https://api.github.com/repos/slavaGanzin/await/tags" | grep " *\"name\":" | head -n1 | sed -E 's/^.*: ?\"v?([0-9\.\-]+)\",?/\1/')
 
-echo "[     await    ] found version ${PKG_VERSION}, updating package.conf..."
+echo "[     await      ] found version ${PKG_VERSION}, updating package.conf..."
 
 sed -i "s/^VERSION=[0-9\.\-]*$/VERSION=${PKG_VERSION}/gm" packages/await/package.conf
 

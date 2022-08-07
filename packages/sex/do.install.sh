98ba37a6c25dce6af63b54a851a3af7a1c36bbab
Emily <36363495+em1lyy@users.noreply.github.com>
2022-08-07 15:58:43 +0200

    more sex fixes

--- packages/sex/do.install.sh
+++ packages/sex/do.install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-install -Dv -m755 sex "${DESTDIR}/usr/bin"
-install -Dv -m644 sex.6 "${DESTDIR}/usr/share/man"
+mkdir -vp "${DESTDIR}/usr/bin"
+mkdir -vp "${DESTDIR}/usr/share/man"
+install -v -m755 sex "${DESTDIR}/usr/bin"
+install -v -m644 sex.6 "${DESTDIR}/usr/share/man"

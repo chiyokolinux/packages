774aa03812f65b05b1013facda6c73b2f300a164
Emily <36363495+em1lyy@users.noreply.github.com>
2022-08-07 15:55:05 +0200

    sex fixes

--- packages/sex/do.build.sh
+++ packages/sex/do.build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+sed -ie '15 i #include <string.h>' sex.c
 cc -O2 $CFLAGS -o sex sex.c $LDFLAGS
--- packages/sex/do.install.sh
+++ packages/sex/do.install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-install -v -m755 sex "${DESTDIR}/usr/bin"
-install -v -m644 sex.6 "${DESTDIR}/usr/share/man"
+install -Dv -m755 sex "${DESTDIR}/usr/bin"
+install -Dv -m644 sex.6 "${DESTDIR}/usr/share/man"
